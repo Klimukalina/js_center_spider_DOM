a244deba15114308653d69ecc528a22e2297c065
Alina <alinaklimuk04@gmail.com>
2024-07-04 17:38:03 +0300

    make build

---
+++ index.9d6bb544.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider");e.style.position="fixed",e.style.left="50%",e.style.top="50%",e.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.9d6bb544.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.position = "fixed";
+spider.style.left = "50%";
+spider.style.top = "50%";
+spider.style.transform = "translate(-50%, -50%)";
+
+//# sourceMappingURL=index.f75de5e1.js.map
